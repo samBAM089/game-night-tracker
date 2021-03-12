e3cc33524271d44224edf13a4f271b020b04c81e
Samuel N <samuel.ngo@me.com>
2021-03-12 12:03:46 +0100

    Init backend

---
+++ backend/src/main/java/de/sambam/gamenighttracker/GameNightTrackerApplication.java
@@ -0,0 +1,13 @@
+package de.sambam.gamenighttracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GameNightTrackerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GameNightTrackerApplication.class, args);
+    }
+
+}
---
+++ backend/src/test/java/de/sambam/gamenighttracker/GameNightTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package de.sambam.gamenighttracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GameNightTrackerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
