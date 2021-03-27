bff102c555778b561e1ca67ae53db95ceb704e18
Samuel N <samuel.ngo@me.com>
2021-03-28 00:24:43 +0100

    Add remaining null-pointer handling

--- backend/src/main/java/de/sambam/gamenighttracker/service/UserService.java
+++ backend/src/main/java/de/sambam/gamenighttracker/service/UserService.java
@@ -33,44 +33,87 @@ public class UserService {
 
     public List<GameSession> listAllSessions(String id) {
         Optional<User> user = userDb.findById(id);
+        List<GameSession> sessionList = new ArrayList<>();
         if (user.isPresent()) {
             List<Game> gameList = user.get().getPlayedGames();
             if (gameList != null) {
-                return gameList.stream()
-                        .filter(game -> game != null)
-                        .flatMap(game -> game.getGameSessionList().stream())
-                        .collect(Collectors.toList());
+                for (Game game : gameList) {
+                    if (game != null) {
+                        List<GameSession> existingSessionList = game.getGameSessionList();
+                        for (GameSession session : existingSessionList) {
+                            if (session != null) {
+                                sessionList.add(session);
+                            }
+                        }
+                    }
+                }
             }
         }
-        return List.of();
+        return sessionList;
     }
 
     public List<PlayerDto> listAllPlayers(String id) {
         Optional<User> user = userDb.findById(id);
+        Map<String, PlayerDto> playerList = new HashMap<>();
         if (user.isPresent()) {
-            List<Game> gameList = userDb.findById(id).get().getPlayedGames();
-            Map<String, PlayerDto> playerMap = new HashMap<>();
+            List<Game> gameList = user.get().getPlayedGames();
             if (gameList != null) {
-                List<Player> playerList = gameList.stream()
-                        .filter(game -> game != null)
-                        .flatMap(game -> game.getGameSessionList().stream())
-                        .filter(session -> session != null)
-                        .flatMap(session -> session.getPlayerList().stream())
-                        .filter(player -> player != null)
-                        .collect(Collectors.toList());
-                for (Player player : playerList) {
-                    if (player != null) {
-                        playerMap.put(player.getName(), PlayerDto.builder()
-                                .name(player.getName())
-                                .color(player.getColor())
-                                .build());
+                for (Game game : gameList) {
+                    if (game != null) {
+                        List<GameSession> existingSessionList = game.getGameSessionList();
+                        if (existingSessionList != null) {
+                            for (GameSession session : existingSessionList) {
+                                if (session != null) {
+                                    List<Player> existingPlayerList = session.getPlayerList();
+                                    if (existingPlayerList != null)
+                                        for (Player player : existingPlayerList) {
+                                            if (player != null) {
+                                                playerList.put(player.getName(), PlayerDto.builder()
+                                                        .name(player.getName())
+                                                        .color(player.getColor())
+                                                        .build());
+
+                                            }
+                                        }
+                                }
+                            }
+                        }
                     }
                 }
-                return playerMap.values().stream().collect(Collectors.toList());
             }
         }
-        return List.of();
+        return playerList.values().stream().collect(Collectors.toList());
     }
+
+       /* public List<PlayerDto> listAllPayers (String id){
+            Optional<User> user = userDb.findById(id);
+            if (user.isPresent()) {
+                List<Game> gameList = userDb.findById(id).get().getPlayedGames();
+                Map<String, PlayerDto> playerMap = new HashMap<>();
+                if (gameList != null) {
+                    List<GameSession> sessionList = gameList.stream()
+                            .filter(game -> game != null)
+                            .flatMap(game -> game.getGameSessionList().stream())
+                            .collect(Collectors.toList());
+                    if (sessionList != null) {
+                        List<Player> playerList = sessionList.stream()
+                                .filter(session -> session != null)
+                                .flatMap(session -> session.getPlayerList().stream())
+                                .collect(Collectors.toList());
+                        for (Player player : playerList) {
+                            if (player != null) {
+                                playerMap.put(player.getName(), PlayerDto.builder()
+                                        .name(player.getName())
+                                        .color(player.getColor())
+                                        .build());
+                            }
+                        }
+                        return playerMap.values().stream().collect(Collectors.toList());
+                    }
+                }
+            }
+            return List.of();
+        }*/
 
     public Optional<Game> addNewGame(Game newGame, String id) {
 
@@ -96,9 +139,12 @@ public class UserService {
                         .filter(game -> game.getApiGameId().equals(apiGameId))
                         .findFirst();
                 if (match.isPresent()) {
-                    match.get().getGameSessionList().add(newSession);
-                    userDb.save(user.get());
-                    return Optional.of(newSession);
+                    List<GameSession> sessionList = match.get().getGameSessionList();
+                    if (sessionList != null) {
+                        sessionList.add(newSession);
+                        userDb.save(user.get());
+                        return Optional.of(newSession);
+                    }
                 }
             }
         }
