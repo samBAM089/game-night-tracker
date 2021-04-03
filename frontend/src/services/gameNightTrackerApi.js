3a6133709420c5dfd67fcd5935df9d11811327d2
Samuel N <samuel.ngo@me.com>
2021-04-03 11:48:30 +0200

    Add changes to components for proper listing of sessions

--- frontend/src/services/gameNightTrackerApi.js
+++ frontend/src/services/gameNightTrackerApi.js
@@ -4,3 +4,8 @@ export const getAllGames = (token) =>
     axios
         .get('user/games', { headers: { Authorization: 'Bearer ' + token } })
         .then((response) => response.data);
+
+export const getAllSessions = (token) =>
+    axios
+        .get('user/sessions', { headers: { Authorization: 'Bearer ' + token } })
+        .then((response) => response.data);
