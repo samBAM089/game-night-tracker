ee976872cd7d3cfa13c4cfd0206de6d38a78e9e2
Samuel N <samuel.ngo@me.com>
2021-04-13 11:04:28 +0200

    Add changes to JwtUtilsTest

--- backend/src/test/java/de/sambam/gamenighttracker/security/JwtUtilsTest.java
+++ backend/src/test/java/de/sambam/gamenighttracker/security/JwtUtilsTest.java
@@ -6,9 +6,11 @@ import io.jsonwebtoken.Jwts;
 import org.hamcrest.Matchers;
 import org.junit.jupiter.api.Test;
 
+import java.time.Duration;
 import java.time.Instant;
 import java.util.Date;
 import java.util.HashMap;
+import java.util.Map;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
@@ -17,26 +19,29 @@ import static org.mockito.Mockito.when;
 
 class JwtUtilsTest {
 
+    private TimeUtils timeUtils = mock(TimeUtils.class);
+    private JwtConfig jwtConfig = mock(JwtConfig.class);
+    private JwtUtils jwtUtils = new JwtUtils(timeUtils, jwtConfig);
+
+
     @Test
     public void generateJwtToken() {
         //GIVEN
-        TimeUtils timeUtils = mock(TimeUtils.class);
-        when(timeUtils.now()).thenReturn(Instant.ofEpochSecond(1616960003L));
+        Instant now = Instant.ofEpochSecond(Instant.now().getEpochSecond());
+        when(timeUtils.now()).thenReturn(now);
         String secret = "super-secret";
-        JwtConfig jwtConfig = mock(JwtConfig.class);
-        when(jwtConfig.getJwtSecret()).thenReturn("super-secret");
+        when(jwtConfig.getJwtSecret()).thenReturn(secret);
 
         JwtUtils jwtUtils = new JwtUtils(timeUtils, jwtConfig);
 
         //WHEN
-        String token = jwtUtils.createToken("sanne", new HashMap<>());
+        String token = jwtUtils.createToken("sanne", new HashMap<>(Map.of("data", "value")));
 
         //THEN
-        Claims claims = Jwts.parser().setSigningKey("top-secret").parseClaimsJws(token).getBody();
+        Claims claims = Jwts.parser().setSigningKey(secret).parseClaimsJws(token).getBody();
         assertThat(claims.getSubject(), Matchers.is("sanne"));
-        assertThat(claims.getIssuedAt(), Matchers.is(new Date(1616960003000L)));
-        int fourHoursInMilliSeconds = 4 * 60 * 60 * 1000;
-        assertThat(claims.getExpiration(), Matchers.is(new Date(1616960003000L + fourHoursInMilliSeconds)));
+        assertThat(claims.getIssuedAt(), Matchers.is(Date.from(now)));
+        assertThat(claims.getExpiration(), Matchers.is(Date.from(now.plus(Duration.ofHours(4)))));
 
     }
 
